b5205bff9bdf5b8f0f0f787e9836ea8af4e11cb5
gluten <gluten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-01 02:23:06 +0000

    updating svn version to be in line with github
    git-svn-id: http://svn.wp-plugins.org/galleria-galleria/trunk@367767 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- galleria-themes/classic/galleria.classic.js
+++ galleria-themes/classic/galleria.classic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Galleria Classic Theme v. 1.5 2010-10-28
  * http://galleria.aino.se
@@ -5,12 +6,24 @@
  * Copyright (c) 2010, Aino
  * Licensed under the MIT license.
  */
+=======
+/**
+ * @preserve Galleria Classic Theme 2011-02-14
+ * http://galleria.aino.se
+ *
+ * Copyright (c) 2011, Aino
+ * Licensed under the MIT license.
+ */
+ 
+/*global jQuery, Galleria */
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
 
 (function($) {
 
 Galleria.addTheme({
     name: 'classic',
     author: 'Galleria',
+<<<<<<< HEAD
     version: '1.5',
     css: 'galleria.classic.css',
     defaults: {
@@ -19,6 +32,15 @@ Galleria.addTheme({
         
 		// set this to false if you want to show the caption all the time:
         _toggle_info: true
+=======
+    css: 'galleria.classic.css',
+    defaults: {
+        transition: 'slide',
+        thumbCrop:  'height',
+        
+		// set this to false if you want to show the caption all the time:
+        _toggleInfo: true
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
     },
     init: function(options) {
         
@@ -29,6 +51,7 @@ Galleria.addTheme({
         });
         
         // cache some stuff
+<<<<<<< HEAD
         var toggle   = this.$('image-nav-left,image-nav-right,counter'),
             info     = this.$('info-link,info-close,info-text'),
             click    = Galleria.TOUCH ? 'touchstart' : 'click';
@@ -46,12 +69,24 @@ Galleria.addTheme({
                 $(this).not('.active').children().stop().fadeTo(400, .6);
             });
             
+=======
+        var info = this.$('info-link,info-close,info-text'),
+            touch = Galleria.TOUCH,
+            click = touch ? 'touchstart' : 'click';
+        
+        // show loader & counter with opacity
+        this.$('loader,counter').show().css('opacity', 0.4);
+
+        // some stuff for non-touch browsers
+        if (! touch ) {
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
             this.addIdleState( this.get('image-nav-left'), { left:-50 });
             this.addIdleState( this.get('image-nav-right'), { right:-50 });
             this.addIdleState( this.get('counter'), { opacity:0 });
         }
         
         // toggle info
+<<<<<<< HEAD
         if ( options._toggle_info ) {
             info.bind( click, function() {
                 info.toggle();
@@ -66,17 +101,59 @@ Galleria.addTheme({
         this.bind(Galleria.LOADSTART, function(e) {
             if (!e.cached) {
                 this.$('loader').show().fadeTo(200, .4);
+=======
+        if ( options._toggleInfo === true ) {
+            info.bind( click, function() {
+                info.toggle();
+            });
+        } else {
+			info.show();
+			this.$('info-link, info-close').hide();
+		}
+        
+        // bind some stuff
+        this.bind('thumbnail', function(e) {
+            
+            if (! touch ) {
+                // fade thumbnails
+                $(e.thumbTarget).css('opacity', 0.6).parent().hover(function() {
+                    $(this).not('.active').children().stop().fadeTo(100, 1);
+                }, function() {
+                    $(this).not('.active').children().stop().fadeTo(400, 0.6);
+                });
+                
+                if ( e.index === options.show ) {
+                    $(e.thumbTarget).css('opacity',1);
+                }
+            }
+        });
+        
+        this.bind('loadstart', function(e) {
+            if (!e.cached) {
+                this.$('loader').show().fadeTo(200, 0.4);
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
             }
             
             this.$('info').toggle( this.hasInfo() );
             
+<<<<<<< HEAD
             $(e.thumbTarget).css('opacity',1).parent().siblings().children().css('opacity',.6);
         });
         
         this.bind(Galleria.LOADFINISH, function(e) {
+=======
+            $(e.thumbTarget).css('opacity',1).parent().siblings().children().css('opacity', 0.6);
+        });
+        
+        this.bind('loadfinish', function(e) {
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
             this.$('loader').fadeOut(200);
         });
     }
 });
 
+<<<<<<< HEAD
 })(jQuery);
+=======
+}(jQuery));
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
--- readme.txt
+++ readme.txt
@@ -4,12 +4,17 @@ Donate link: https://github.com/samargulies/galleria-galleria/
 Tags: photo, photos, gallery, galleries, photo gallery, lightbox 
 Requires at least: 3.0
 Tested up to: 3.1
+<<<<<<< HEAD
 Stable tag: 0.1a
+=======
+Stable tag: 0.1
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
 
 Transform standard WordPress galleries into galleria slideshows. 
 
 == Description ==
 
+<<<<<<< HEAD
 Galleria Galleria is a fork of [Photo Galleria](http://wordpress.org/extend/plugins/photo-galleria/) that is designed to minimize resources and selectively load javascript. I love how the Photo Galleria plugin was integrated into the Wordpress gallery system but I could not stand seeing so many resources load on every page when only one might contain a gallery. Galleria Galleria will only load javascript on pages that actually have a gallery. Beyond that, we will see what other improvements I can make to this plugin. I'm open for suggestions!
 
 
@@ -23,6 +28,28 @@ Galleria Galleria is a fork of [Photo Galleria](http://wordpress.org/extend/plug
 
 == Frequently Asked Questions ==
 
+=======
+Galleria Galleria is a fork of [Photo Galleria](http://wordpress.org/extend/plugins/photo-galleria/) that is designed to minimize resources and selectively load javascript. This plugin will only load javascript on pages that actually have a gallery. Galleria Galleria also keeps standard gallery styling if javascript is not enabled.  Beyond that, this plugin is built using the [Galleria javascript library](http://galleria.aino.se/) that allows for advanced theming and customization. 
+
+== Installation ==
+
+1. Upload the entire `galleria-galleria` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. All existing galleries inserted using the [gallery] shortcode will now use Galleria Galleria.
+
+== Screenshots ==
+
+1. The Galleria Galleria settings page
+2. An example gallery
+== Frequently Asked Questions ==
+
+= How is this plugin different from other Wordpress gallery plugins? =
+Galleria Galleria is designed to only selectively load javascript to reduce the load time of your site. It also works seamlessly with galleries you have already created for your site in Wordpress.
+
+== Other Notes ==
+This plugin uses the standard [gallery] shortcode to create slideshows. It also adds support for a per gallery height attribute in the format [gallery height=500] to create a gallery with a height of 500px.
+
+>>>>>>> 35c5c655cc18e3740580345c3e138092eb06e20d
 == Changelog ==
 
 = Version 0.1 =
